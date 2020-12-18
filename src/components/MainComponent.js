26adbdea11bbf05ac4b0c374a61bb5a4294241c0
NadezhdaBabenko <chestnaya.n@gmail.com>
2020-12-18 17:46:41 +0100

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react"; // создает компонент - потом его надо импортировать в App.js
 import Home from './HomeComponent'; 
 import Menu from './MenuComponent'; //импортируем каждый компонент, указывая путь до него и потом пишешь его в коду ниже как <Menu />  
+import Contact from './ContactComponent';
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes'; 
-import { Switch, Route, Redirect } from 'react-router-dom'; 
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions'; 
+
+
+
+import { Switch, Route, Redirect } from 'react-router-dom';  //для роутинга 
 
 class Main extends Component {
   
@@ -15,6 +22,9 @@ class Main extends Component {
     this.state = {
       dishes: DISHES,
     //   selectedDish: null
+    comments: COMMENTS,
+    promotions: PROMOTIONS,
+    leaders: LEADERS
     };
   }
 
@@ -28,11 +38,15 @@ class Main extends Component {
 //  <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />  
 //  <Dishdetail state={this.state} dish={this.state.dishes.find((dish) => dish.id === this.state.selectedDish)} /> 
 // <Switch> - для переключения - роутинг
+//<Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} /> - для всех dishes у кого featured = true - выделятся эти только -> [0]-толкьо первый эемент
 render() {
 
     const HomePage = () => {
         return(
-            <Home />
+            <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}//передает состояние (state) dishes - фильтрует и берет тот объект у которого у первого встречается ключ featured как true ---> дальше передает в HomeCmp как входящий параметр <RenderCard item={props.dish}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]} //передает состояние (state) promotion - фильтрует и берет тот объект у которого у первого встречается ключ featured как true ---> дальше передает в HomeCmp как входящий параметр <RenderCard item={props.promotion}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]} //передает состояние (state) leaders - фильтрует и берет тот объект у которого у первого встречается ключ featured как true ---> дальше передает в HomeCmp как входящий параметр <RenderCard item={props.leader}
+            />
         );
     } 
     return (
@@ -41,6 +55,7 @@ render() {
             <Switch>
                 <Route path="/home" component={HomePage} />
                 <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                <Route exact path="/contactus" component={Contact} />
                 <Redirect to="/home" /> 
             </Switch>
             <Footer />
