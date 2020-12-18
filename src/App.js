4801f853f1780529841a3abedac09d11bd7e8fd7
NadezhdaBabenko <chestnaya.n@gmail.com>
2020-12-18 16:22:58 +0100

    React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react"; // создает компонент - потом его надо импортировать в App.js
 import Main from './components/MainComponent'; //импортируем каждый компонент, указывая путь до него и потом пишешь его в коду ниже как <Menu />  
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Main /> 
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main /> 
+        </div>
+      </BrowserRouter>
     );
   } 
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react"; // создает компонент - потом его надо импортировать в App.js
+import Home from './HomeComponent'; 
 import Menu from './MenuComponent'; //импортируем каждый компонент, указывая путь до него и потом пишешь его в коду ниже как <Menu />  
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import {DISHES} from '../shared/dishes'; 
+import { DISHES } from '../shared/dishes'; 
+import { Switch, Route, Redirect } from 'react-router-dom'; 
 
 class Main extends Component {
   
@@ -12,25 +14,45 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null
+    //   selectedDish: null
     };
   }
 
 //onClick={() => this.onDishSelect(dish)} при клике на каждую карточку вызывается ф-ция
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId }); //меняем состояние
-    }
+    // onDishSelect(dishId) {
+    //     this.setState({ selectedDish: dishId }); //меняем состояние
+    // }
+
 //<Menu dishes={this.state.dishes}/> -> dishes={this.state.dishes} - это props
-    render() {
+
+//  <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />  
+//  <Dishdetail state={this.state} dish={this.state.dishes.find((dish) => dish.id === this.state.selectedDish)} /> 
+// <Switch> - для переключения - роутинг
+render() {
+
+    const HomePage = () => {
+        return(
+            <Home />
+        );
+    } 
     return (
         <div>
             <Header />
-            <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} /> 
-            <Dishdetail state={this.state} dish={this.state.dishes.find((dish) => dish.id === this.state.selectedDish)} />
+            <Switch>
+                <Route path="/home" component={HomePage} />
+                <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                <Redirect to="/home" /> 
+            </Switch>
             <Footer />
         </div>
     );
 } 
 }
+
+// <Switch> - переключение между страницами
+// <Route path="/home" component={HomePage} /> - добавляется в юрл путь - и переключается на компонент Хоум
+// <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} /> - добавляется в юрл точный путь Меню и отпрывается компонент <Menu + плюс состояние передается тоже тут/>
+// <Redirect to="/home" /> //если выбирается что то другое- чего нет оно перенаправляет на страницу Хоум (вкладки меню которых еще нет)
+// </Switch> 
 
 export default Main;
